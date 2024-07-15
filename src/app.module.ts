41345b519f66ea6aacf2c25662e960c4746492be
FILIMON <f.rykov@bk.ru>
2024-07-15 17:43:33 +0300

    Setting up data base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      envFilePath: '.env',
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'admin',
+      entities: [],
+      synchronize: false,
+    }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
