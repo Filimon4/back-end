812c260565701244eeb4de853cc3d07e57917b45
FILIMON <f.rykov@bk.ru>
2024-07-26 22:59:26 +0300

    init front-end

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,20 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'admin',
-      entities: [],
-      synchronize: false,
-    }),
-    UsersModule,
-  ],
+  imports: [UsersModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'postgres',
+    password: 'admin',
+    database: 'notion_db',
+    entities: [
+      __dirname + 'back-end/**/*.entity.{ts,js}'
+    ],
+    synchronize: false
+  })],
   controllers: [],
   providers: [],
 })
